fe979e052bfb5babf98be60f325b4902d9e4b45f
remor61 <eduardo.remors@gmail.com>
2022-02-23 18:18:22 -0300

    starting with components

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -31,9 +31,9 @@ module.exports = {
         use: "babel-loader", // sets what will be used for the conversion
       },
       {
-        test: /\.css$/i,
+        test: /\.scss$/i,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
