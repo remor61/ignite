aa214ec2354d5d48df0058db5a31ec50e1ba852d
remor61 <eduardo.remors@gmail.com>
2022-03-05 21:45:15 -0300

    switched to typescript

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,38 +1,38 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: isDevelopment ? "development" : "production",
-  devtool: isDevelopment ? "eval-source-map" : "source-map",
-  entry: path.resolve(__dirname, "src", "index.jsx"), //__dirname gets the directory of this file.
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'), //__dirname gets the directory of this file.
   output: {
     // here the file that webpack will generate is set
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js", ".jsx"], // here we say to webpack that it can work with .js and .jsx fles
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // here we say to webpack that it can work with .js and .jsx fles
   },
   devServer: {
-    static: path.resolve(__dirname, "public"),
+    static: path.resolve(__dirname, 'public'),
     hot: true,          //added with ReactRefreshWebpackPlugin
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ].filter(Boolean),
   module: {
     rules: [
       {
-        test: /\.jsx$/, // regula expression to get only .jsx files
-        exclude: /node_modules/, // excludes everythin in node_modules as those files are already "browser ready"
+        test: /\.(j|t)sx$/, // regula expression to get only .jsx files
+        exclude: /node_modules/, // excludes everythin in node_modules as those files are already 'browser ready'
         use: {
-          loader: "babel-loader", // sets what will be used for the conversion
+          loader: 'babel-loader', // sets what will be used for the conversion
           options: {    //added with ReactRefreshWebpackPlugin
             plugins: [
               isDevelopment && require.resolve('react-refresh/babel')
@@ -43,7 +43,7 @@ module.exports = {
       {
         test: /\.scss$/i,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
