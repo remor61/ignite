bdd0ae08b15d7787ded3e86c0225b02a29fe62c7
Eduardo Remor <eduardo.remor@sap.com>
2022-03-24 22:56:55 -0300

    styled modal

--- reactjs/chapter-2/dtmoney/src/App.tsx
+++ reactjs/chapter-2/dtmoney/src/App.tsx
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import { useState } from "react";
 
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement("#root");
 
@@ -22,12 +23,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
-      <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar Transação</h2>
-      </Modal>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle />
     </>
